039aa7285ffd6e6e2c95f410c37f0fe4554ffdfb
Gopal <gopalurdukhe@gmail.com>
2022-11-27 12:58:40 +0530

    added sop statement

--- src/main/java/in/gopalit/ProducAppt.java
+++ src/main/java/in/gopalit/ProducAppt.java
@@ -3,6 +3,7 @@ package in.gopalit;
 public class ProducAppt {
 	public static void main(String[] args) {
 		System.out.println("welcome to product app");
+		System.out.println("we have many offer");
 	}
 
 }
